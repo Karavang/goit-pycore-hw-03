e8370c9307471cf0c47181a211cf06f5012cf0b3
Karavang <shestakevgeny10@gmail.com>
2025-03-11 02:43:42 +0100

    normalize_phone

--- index.py
+++ index.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from random import randint
+import re
 
 
 def get_days_from_today(date):
@@ -11,9 +12,29 @@ def get_days_from_today(date):
 
 
 def get_numbers_ticket(min, max, quantity):
+    # Створюємо множину для того, щоб не було повторень
     arr = set()
+    # Робимо перевірку на можливість вибору такої кількості чисел
     if quantity > max - min:
         return []
+    # Якщо все круто - генеруємо числа та віддаємо їх у вигляді списку
     while len(arr) != quantity:
         arr.add(randint(min, max))
     return list(arr)
+
+
+def normalize_phone(phone_number):
+    number = ""
+    pattern = r"\d+"
+    matches = re.findall(pattern, phone_number)
+    print(str(matches))
+    for match in matches:
+        number += match
+    if phone_number[0] == "+" or phone_number[0:3] == "380":
+        number = "+" + number
+    else:
+        number = "+38" + number
+    return number
+
+
+print(normalize_phone("380 67 777 77 aboba77"))
