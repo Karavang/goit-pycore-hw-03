d41f7a2b83cbbbf1aeeda55c58da8129beb76cd5
Karavang <shestakevgeny10@gmail.com>
2025-03-11 16:01:28 +0100

    get_upcoming_birthdays

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from random import randint
 import re
 
@@ -37,4 +37,31 @@ def normalize_phone(phone_number):
     return number
 
 
-print(normalize_phone("380 67 777 77 aboba77"))
+def get_upcoming_birthdays(users):
+    today = datetime.today().date()
+    upcoming_birthdays = []
+
+    for user in users:
+        name = user["name"]
+        birthday = datetime.strptime(user["birthday"], "%Y.%m.%d").date()
+        birthday_this_year = birthday.replace(year=today.year)
+
+        # Якщо день народження вже минув у цьому році, перевіряємо наступний рік
+        if birthday_this_year < today:
+            birthday_this_year = birthday.replace(year=today.year + 1)
+
+        # Перевіряємо, чи день народження у наступні 7 днів
+        if 0 <= (birthday_this_year - today).days <= 7:
+            congrats_date = birthday_this_year
+            # Якщо день народження випадає на вихідний, переносимо на понеділок
+            if congrats_date.weekday() in (5, 6):
+                congrats_date += timedelta(days=(7 - congrats_date.weekday()))
+
+            upcoming_birthdays.append(
+                {
+                    "name": name,
+                    "congratulation_date": congrats_date.strftime("%Y.%m.%d"),
+                }
+            )
+
+    return upcoming_birthdays
